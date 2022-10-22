ed33bd0294a90b53430548996dca3de317e22c4d
anujnaruka02 <73575537+anujnaruka02@users.noreply.github.com>
2022-10-22 18:14:46 +0530

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -1,3 +1,4 @@
+# For first confusion matrix
 import tensorflow as tf
 from tensorflow import keras
 
@@ -10,5 +11,16 @@ model.fit(X_train, Y_train, epochs = 50)
 
 model.evaluate(X_test, Y_test)
 
+# For second confusion matrix
+import tensorflow as tf
+from tensorflow import keras
 
+model = keras.Sequential([keras.layers.Dense(20, input_shape = (26,), activation = 'relu'),
+                         keras.layers.Dense(1, activation = 'sigmoid')])
+
+model.compile(optimizer = 'adagrad', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+model.fit(X_train, Y_train, epochs = 50)
+
+model.evaluate(X_test, Y_test)
 
